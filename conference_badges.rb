799709985573368c5d7d6c8a36a563ff9e11a8f5
Wesley <wspencer3295@yahoo.com>
2020-11-11 13:04:03 -0700

    commit tree c9174977a5e02a5e9452edf58098698e0ef82730

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+
+
+def batch_badge_creator(attendees)
+new_arr = []
+attendees.each do |name|
+    new_arr << "Hello, my name is #{name}."
+    
+end
+new_arr
+end
+
+
+def assign_rooms(speakers)
+    new_arr = []
+    speakers.each_with_index do |name, i|
+        new_arr << "Hello, #{name}! You'll be assigned to room #{i +1}!"
+    end
+    new_arr
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |creator| 
+        puts creator
+    end
+    assign_rooms(attendees).each do |speak|
+        puts speak
+    end      
+end
